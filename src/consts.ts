13f15ca9e851f7ed95357085f3e3908bab33690b
Miguel Ángel Durán <miduga@gmail.com>
2024-05-23 13:45:35 +0200

    Add files

---
+++ src/consts.ts
@@ -0,0 +1,118 @@
+export const CLASES = [
+  {
+    "id": "7iobxzd_2wY",
+    "slug": "01-inicio",
+    "title": "¿Qué es React? Desde cero",
+    "description": "Entiende qué es, para qué sirve y cómo funciona React. Desde lo más básico.",
+    "learnings": ["Desde cero", "JSX", "props"],
+    "code": "https://github.com/midudev/aprendiendo-react/blob/master/projects/01-twitter-follow-card",
+    "web": "https://midu-react-01.surge.sh"
+  },
+  { 
+    "id": "qkzcjwnueLA",
+    "slug": "02-use-state-use-effect",
+    "title": "Eventos, efectos y estados en React",
+    "description": "Creamos un par de juegos para aprender a usar useState, useEffect y manejo de eventos en React",
+    "learnings": ["useState", "useEffect", "eventos"],
+    "code": "https://github.com/midudev/aprendiendo-react/blob/master/projects/02-tic-tac-toe",
+    "web": "https://midu-react-02.surge.sh"
+  },
+  {
+    "id": "XYpadB4VadY",
+    "slug": "03-react-prueba-tecnica",
+    "title": "Prueba técnica con Promesas, fetching y testing E2E",
+    "code": "https://github.com/midudev/aprendiendo-react/blob/master/projects/04-react-prueba-tecnica",
+    "description": "Solucionamos una prueba técnica de React con asincronía y testing para desarrolladores Junior",
+    "learnings": ["Promesas", "API", "testing"],
+    "web": "https://midu-react-04.surge.sh"
+  },
+  {
+    "id": "LcbVw99o8",
+    "slug": "04-custom-hooks",
+    "title": "Crea tus propios hooks en React + Testing E2E",
+    "description": "Aprende a reutilizar código en tus componentes con los Custom Hooks y cómo usar Playwright para testear tus componentes",
+    "code": "https://github.com/midudev/aprendiendo-react/blob/master/projects/04-react-prueba-tecnica",
+    "learnings": ["Custom Hooks", "testing"],
+    "web": "https://midu-react-04.surge.sh"
+  },
+  {
+    "id": "GOEiMwDJ3lc",
+    "slug": "05-use-callback-use-memo-use-ref", 
+    "title": "useCallback, useMemo y useRef",
+    "description": "Descubre más hooks para mejorar el rendimiento de tus componentes en React",
+    "learnings": ["useCallback", "useMemo", "useRef"],
+    "code": "https://github.com/midudev/aprendiendo-react/tree/master/projects/05-react-buscador-peliculas",
+    "web": "https://midu-react-05.surge.sh"    
+  },
+  {
+    "id": "B9tDYAZZxcE",
+    "slug": "06-use-reducer-use-context",
+    "title": "Estado global con useContext y useReducer",
+    "description": "Aprende a crear un estado global al que puedes acceder desde cualquier parte de tu aplicación gracias a la Context API",
+    "code": "https://github.com/midudev/aprendiendo-react/blob/master/projects/06-shopping-cart",
+    "learnings": ["estado global", "useContext", "useReducer"],
+    "web": "https://midu-react-06.surge.sh"
+  },
+  {
+    "id": "K2NcGYajvY4",
+    "slug": "07-midu-router",
+    "title": "Creación de un React Router desde cero",
+    "description": "Crea paso a paso y desde cero tu propia biblioteca de router para React",
+    "code": "https://github.com/midudev/aprendiendo-react/blob/master/projects/07-midu-router",
+    "learnings": ["router", "DOM API"],
+    "web": "https://midu-react-07.surge.sh"
+  },
+  {
+    "id": "4lAYfsq-2TE",
+    "slug": "08-todo-app-typescript",
+    "title": "Todo App con TypeScript y animaciones",
+    "description": "Usamos TypeScript para crear una aplicación de tareas con React, añadiendo animaciones fácilmente",
+    "code": "https://github.com/midudev/aprendiendo-react/blob/master/projects/08-todo-app-typescript",
+    "learnings": ["TypeScript", "Animaciones"],
+    "web": "https://midu-react-08.surge.sh"
+  },
+  {
+    "id": "kZhabulNCUc",
+    "slug": "09-google-translate-clone",
+    "title": "Crear un Google Translate con ChatGPT y TypeScript",
+    "description": "Usamos ChatGPT para crear un traductor de texto con React y TypeScript",
+    "code": "https://github.com/midudev/aprendiendo-react/blob/master/projects/09-google-translate-clone",
+    "learnings": ["ChatGPT", "TypeScript"],
+    "web": "https://midu-react-09.surge.sh"
+  },
+  {
+    "id": "bEEjuwujbbU",
+    "slug": "10-crud-redux",
+    "title": "Crear un CRUD con Redux Toolkit y TypeScript",
+    "description": "El estilo de aplicación más típico solucionado usando un estado global con Redux Toolkit",
+    "code": "https://github.com/midudev/aprendiendo-react/blob/master/projects/10-crud-redux",
+    "learnings": ["CRUD", "Redux", "Estado global"],
+    "web": "https://midu-react-10.surge.sh"
+  },
+  {
+    "id": "mNJOWXc83Y4",
+    "slug": "11-typescript-prueba-tecnica",
+    "title": "Prueba Técnica con TypeScript y React",
+    "description": "Sube de nivel resolviendo esta prueba técnica en el que tienes que listar una tabla y añadir filtros",
+    "code": "https://github.com/midudev/aprendiendo-react/blob/master/projects/11-typescript-prueba-tecnica",
+    "learnings": ["TypeScript", "Performance"],
+    "web": "https://midu-react-11.surge.sh"
+  },
+  {
+    "id": "WKfVjQUa6nE",
+    "slug": "12-comments-react-query",
+    "title": "Sistema de comentarios con React Query",
+    "description": "Aprende a crear un sistema de comentarios con React Query",
+    "code": "https://github.com/midudev/aprendiendo-react/blob/master/projects/12-comments-react-query",
+    "learnings": ["React Query", "API"],
+  },
+  {
+    "id": "p2wF2wRjcN0",
+    "slug": "13-javascript-quiz-con-zustand",
+    "title": "JavaScript Quiz con Zustand y TypeScript",
+    "code": "https://github.com/midudev/aprendiendo-react/blob/master/projects/13-javascript-quiz-con-zustand",
+    "web": "https://midu-react-13.surge.sh",
+    "description": "Usamos Zustand para crear una aplicación de quiz con React y TypeScript",
+    "learnings": ["Zustand", "TypeScript"]
+  }
+] as const 
