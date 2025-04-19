b6bd8d62b51ea4cd712ab4f04fa289bbfba77d8a
midudev <miduga@gmail.com>
2025-04-19 10:25:22 +0200

    Add MDX support and content for React course lessons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()]
   }
