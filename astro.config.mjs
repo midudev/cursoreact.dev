a040c09d550398accfbbba3f48d1c50149afae82
midudev <miduga@gmail.com>
2025-04-21 10:10:55 +0200

    Add inlineStylesheets build option to always inline CSS styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: 'always'
+  },
   integrations: [mdx(), sitemap()],
   site: 'https://cursoreact.dev',
   vite: {
