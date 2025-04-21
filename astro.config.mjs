019cb2763c88397a7358c6e0800a15543ae0ca04
midudev <miduga@gmail.com>
2025-04-21 10:08:36 +0200

    Add sitemap integration and remove console.log from clase page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
+  site: 'https://cursoreact.dev',
   vite: {
     plugins: [tailwindcss()]
   }
